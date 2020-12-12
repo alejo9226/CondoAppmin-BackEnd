403da9017c297438161fc4793bdf7a43bd9138e8
Alejandro Alfaro <alejo9226@gmail.com>
2020-12-12 14:36:09 -0500

    Fix ticket show controller to throw error when getting queries with other than read key

--- src/routes/unit.js
+++ src/routes/unit.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const unitController = require("../controllers/unit.controller");
+const { auth } = require("../utils/auth");
 
+router.use(auth)
 router.route("/").post(unitController.create);
 router.route("/").get(unitController.list);
 router.route("/:condoid").get(unitController.show);
+router.route("/").delete(unitController.deleteAll);
 
 module.exports = router;
